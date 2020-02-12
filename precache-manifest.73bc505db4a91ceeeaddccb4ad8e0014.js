8e177786644ff4bdc508395d7d0448ab4c08c22d
navi35 <onder.ogretmen.35@gmail.com>
2020-02-13 00:00:01 +0300

    deploy

---
+++ precache-manifest.73bc505db4a91ceeeaddccb4ad8e0014.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7455bf59a42035c6d9e",
+    "url": "/onderogretmen/css/app.17d0086d.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/onderogretmen/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "c8b3d32e77ef13850a3b263ae49a0bfe",
+    "url": "/onderogretmen/index.html"
+  },
+  {
+    "revision": "663824208dd300109592",
+    "url": "/onderogretmen/js/about.bf82edad.js"
+  },
+  {
+    "revision": "a7455bf59a42035c6d9e",
+    "url": "/onderogretmen/js/app.905eb25f.js"
+  },
+  {
+    "revision": "78185cbc519ba1e04382",
+    "url": "/onderogretmen/js/chunk-vendors.9bbf69e0.js"
+  },
+  {
+    "revision": "9ed58d3b90d779cb9bddd4392ddd0f38",
+    "url": "/onderogretmen/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/onderogretmen/robots.txt"
+  }
+]);
